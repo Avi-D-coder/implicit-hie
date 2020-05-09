c5b02175f349343880617c4ef6a68ec4f4206c29
Avi Dessauer <avi.the.coder@gmail.com>
2020-05-09 03:29:46 -0400

    Correct bench

--- src/Hie/Yaml.hs
+++ src/Hie/Yaml.hs
@@ -32,14 +32,14 @@ cabalComponent :: Name -> Component -> (FilePath, String)
 cabalComponent n (Comp Lib "" p) = (T.unpack p, T.unpack $ "lib:" <> n)
 cabalComponent n (Comp Lib cn p) = (T.unpack p, T.unpack $ "lib:" <> n <> ":" <> cn)
 cabalComponent n (Comp Exe cn p) = (T.unpack p, T.unpack $ n <> ":exe:" <> cn)
-cabalComponent n (Comp Bench cn p) = (T.unpack p, T.unpack $ n <> ":exe:" <> cn)
+cabalComponent n (Comp Bench cn p) = (T.unpack p, T.unpack $ n <> ":bench:" <> cn)
 cabalComponent n (Comp Test cn p) = (T.unpack p, T.unpack $ n <> ":test:" <> cn)
 
 stackComponent :: Name -> Component -> (FilePath, String)
 stackComponent n (Comp Lib "" p) = (T.unpack p, T.unpack $ n <> ":lib")
 stackComponent n (Comp Lib cn p) = (T.unpack p, T.unpack $ n <> ":lib:" <> cn)
 stackComponent n (Comp Exe cn p) = (T.unpack p, T.unpack $ n <> ":exe:" <> cn)
-stackComponent n (Comp Bench cn p) = (T.unpack p, T.unpack $ n <> ":exe:" <> cn)
+stackComponent n (Comp Bench cn p) = (T.unpack p, T.unpack $ n <> ":bench:" <> cn)
 stackComponent n (Comp Test cn p) = (T.unpack p, T.unpack $ n <> ":test:" <> cn)
 
 fmtComponent :: (FilePath, String) -> String
