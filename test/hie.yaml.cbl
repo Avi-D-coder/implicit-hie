3621971ac9d21bcea27bd1715471ffb20d0037de
Avi Dessauer <avi.the.coder@gmail.com>
2022-10-18 23:53:58 -0500

    Fix/delete tests

--- test/hie.yaml.cbl
+++ test/hie.yaml.cbl
@@ -1,22 +1,2 @@
 cradle:
   cabal:
-    - path: "src"
-      component: "lib:haskell-language-server"
-
-    - path: "exe/Main.hs"
-      component: "haskell-language-server:exe:haskell-language-server"
-
-    - path: "exe/Arguments.hs"
-      component: "haskell-language-server:exe:haskell-language-server"
-
-    - path: "exe/Wrapper.hs"
-      component: "haskell-language-server:exe:haskell-language-server-wrapper"
-
-    - path: "exe/Arguments.hs"
-      component: "haskell-language-server:exe:haskell-language-server-wrapper"
-
-    - path: "test/functional"
-      component: "haskell-language-server:test:func-test"
-
-    - path: "test/utils"
-      component: "haskell-language-server:lib:hls-test-utils"
